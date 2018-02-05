c4dd5de8467dadf97478c37789875af98a4e8fd3
Dimitriy <dleksanov@stuy.edu>
2018-02-05 14:00:54 -0500

    altered js

--- canvas.js
+++ canvas.js
@@ -4,26 +4,28 @@ var toggleButt = document.getElementById("toggle");
 var ctx = canvas.getContext('2d');
 var toggleOpt = 0;
 
+/*var opt0 = function(e){
+    
+}*/
+
 var place = function(e){
-    var mouseX = e.x;
-    var mouseY = e.y;
-  
     if(toggleOpt == 0){
 	ctx.fillStyle = "red";
-
 	ctx.beginPath();
-	ctx.arc(mouseX - 5, mouseY - 75, 10, 0, 2 * Math.PI);
+	//ctx.lineTo(mouseX, mouseY);
+	ctx.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI);
+	ctx.stroke();
 	ctx.fill();
     }
     else if(toggleOpt == 1){
 	ctx.fillStyle = "black";
 	
 	ctx.beginPath();
-	ctx.arc(mouseX - 5, mouseY - 75, 15, 0, 2 * Math.PI);
+	ctx.arc(offsetX, offsetY, 15, 0, 2 * Math.PI);
 	ctx.stroke();
     }
     else{ //if toggleOpt is 2
-	ctx.fillRect(mouseX - 5, mouseY - 75, 55, 45);
+	ctx.fillRect(offsetX, offsetY, 55, 45);
     }
 }
 
